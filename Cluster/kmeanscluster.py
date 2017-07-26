39cda7c190843e4fbfcabd16457fd6bb4a7c6d13
pj9039 <kms_14@naver.com>
2017-07-26 19:01:37 +0900

    코드 다이어트, 클러스터 결과 boxplot으로 보여주는 코드 추가

--- Cluster/kmeanscluster.py
+++ Cluster/kmeanscluster.py
@@ -2,69 +2,122 @@ from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from sklearn import preprocessing
 
 NetworkDF = pd.read_csv('../log/(after) Network measures features.csv')
 PinfoDF = pd.read_csv('../log/(after) Player information features.csv')
 PactionDF = pd.read_csv('../log/(after) Player actions features.csv')
+GactiveDF = pd.read_csv('../log/(after) Group activities features.csv')
+SointerDF = pd.read_csv('../log/(after) Social interaction diversity features.csv')
 
-NetworkHumanDF = NetworkDF.loc[NetworkDF['Type'] == "Human"]
-NetworkBotDF = NetworkDF.loc[NetworkDF['Type'] == "Bot"]
-ActionHumanDF = PactionDF.loc[PactionDF['Type'] == "Human"]
-ActionBotDF = PactionDF.loc[PactionDF['Type'] == "Bot"]
+mergetmp = pd.merge(NetworkDF.iloc[:,0:-1], PinfoDF.drop(['A_Acc'], axis =1).iloc[:,0:-1], on='Actor', how='inner')
+mergetmp = pd.merge(mergetmp, PactionDF.drop(['A_Acc'], axis =1).iloc[:,0:-1], on='Actor', how='inner')
+mergetmp = pd.merge(mergetmp, GactiveDF.drop(['A_Acc'], axis =1).iloc[:,0:-1], on='Actor', how='inner')
+mergerDF = pd.merge(mergetmp, SointerDF.drop(['A_Acc'], axis =1), on='Actor', how='inner')
 
-MaxLevelTotal = PinfoDF.query('Max_level == 55')
-MaxLevelHuman = PinfoDF.query('Type == "Human" & Max_level == 55')
-MaxLevelBot = PinfoDF.query('Type == "Bot" & Max_level == 55')
-
-MaxLevelTotal_Actor = MaxLevelTotal[['Actor']]
-MaxLevelHuman_Actor = MaxLevelHuman[['Actor']]
-MaxLevelBot_Actor = MaxLevelBot[['Actor']]
-
-Total_Net_Info_merge = pd.merge(MaxLevelTotal_Actor, NetworkHumanDF, on='Actor', how='inner')
-Human_Net_Info_merge = pd.merge(MaxLevelHuman_Actor, NetworkHumanDF, on='Actor', how='inner')
-Bot_Net_Info_merge = pd.merge(MaxLevelBot_Actor, NetworkBotDF, on='Actor', how='inner')
-
-Total_Action_Info_merge = pd.merge(MaxLevelTotal_Actor, ActionHumanDF, on='Actor', how='inner')
-Human_Action_Info_merge = pd.merge(MaxLevelHuman_Actor, ActionHumanDF, on='Actor', how='inner')
-Bot_Action_Info_merge = pd.merge(MaxLevelBot_Actor, ActionBotDF, on='Actor', how='inner')
+Total_Action_Info_merge = mergerDF.query('Max_level == 55 & Type == "Bot"')
 
 
-feature1 = 'Exp_get_count'
-feature2 = 'Item_get_count'
-feature3 = 'Money_get_count'
-feature4 = 'Abyss_get_count'
-feature5 = 'Exp_repair_count'
-feature6 = 'Killed_bypc_count'
-feature7 = 'Killed_bynpc_count'
-feature8 = 'Teleport_count'
-feature9 = 'Type'
-
-Total_feature1 = Total_Action_Info_merge[feature1]
-Total_feature2 = Total_Action_Info_merge[feature2]
-Total_feature3 = Total_Action_Info_merge[feature3]
-Total_feature4 = Total_Action_Info_merge[feature4]
-Total_feature5 = Total_Action_Info_merge[feature5]
-Total_feature6 = Total_Action_Info_merge[feature6]
-Total_feature7 = Total_Action_Info_merge[feature7]
-Total_feature8 = Total_Action_Info_merge[feature8]
-Total_feature9 = Total_Action_Info_merge[feature9]
+features = ['p_deg', 't_deg','Abyss_get_count','collect_max_count','Exp_get_count', 'Type']
 
 
-frames = [Total_feature1, Total_feature2, Total_feature3, Total_feature4, Total_feature5, Total_feature6, Total_feature7, Total_feature8 ,Total_feature9]
+
+frames = []
+for i in features:
+    frames.append(Total_Action_Info_merge[i])
+
+
 result = pd.concat(frames, axis=1)
 
-k = 4
-X = np.array(result.iloc[:,:-1].values.tolist())
+X = preprocessing.scale(result.iloc[:,:-1].values.tolist())
+
+k = 5
+#X = np.array(result.iloc[:,:-1].values.tolist())
+#X = preprocessing.scale(X)
 kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
 labels = kmeans.labels_
 centroids = kmeans.cluster_centers_
+print(kmeans.cluster_centers_ )
 
 labels = pd.DataFrame(labels)
-labels.columns=['cluster']
 
+labels.columns=['cluster']
+X = pd.DataFrame(X, columns = ['p_deg','t_deg','Abyss_get_count','collect_max_count','Exp_get_count'])
+#X = X.reset_index()
+
+result = pd.DataFrame(result, columns = ['Type'])
+result = result.reset_index(drop=True)
+
+result = pd.concat([X,result], axis=1, join_axes=[result.index])
 cluster_result_df = pd.concat([result, labels], axis=1, join_axes=[result.index])
 
 
+for i in range(0,k):
+    print("----cluster : %s----"%i)
+    print("human: ", cluster_result_df.query('cluster == %s & Type=="Human"'%i).count()[0])
+    print("Bot: ", cluster_result_df.query('cluster == %s & Type=="Bot"'%i).count()[0])
 
+
+
+def MakeHist(clustering1,clustering2, bot_or_Human1, compareV1, bot_or_Human2, compareV2, Save_or_Not=0):
+
+    if (bot_or_Human1 =='Human'):
+        x = cluster_result_df.query('cluster == %s & Type=="Human"'%clustering1)[compareV1]
+    elif(bot_or_Human1 =='Bot'):
+        x = cluster_result_df.query('cluster == %s & Type=="Bot"' % clustering1)[compareV1]
+
+    if (bot_or_Human2 == 'Human'):
+        y = cluster_result_df.query('cluster == %s & Type=="Human"'%clustering2)[compareV2]
+
+    elif (bot_or_Human2 == 'Bot'):
+        y = cluster_result_df.query('cluster == %s & Type=="Bot"' % clustering2)[compareV2]
+
+
+    bins = [0, 1, 2, 3, 4, 5, 10]
+    titlename = '%s_%s & %s_%s'%(bot_or_Human1, compareV1, bot_or_Human2, compareV2)
+    print('%s_%s_%s의 평균'%(bot_or_Human1, compareV1, clustering1),x.mean())
+    print('%s_%s_%s의 표준 편차'%(bot_or_Human1, compareV1, clustering1),x.std())
+    print('%s_%s_%s의 평균'%(bot_or_Human2, compareV2, clustering2),y.mean())
+    print('%s_%s_%s의 표준 편차'%(bot_or_Human2, compareV2, clustering2),y.std())
+    plt.hist(x, bins, alpha=0.5, label='%s_%s_%s'%(bot_or_Human1, compareV1, clustering1), color = "b")
+    plt.hist(y, bins, alpha=0.5, label='%s_%s_%s'%(bot_or_Human2, compareV2, clustering2), color = "r")
+    plt.legend(loc='upper right')
+    plt.title('%s'%titlename)
+    if (Save_or_Not==1):
+        plt.savefig("EDAPicture/Hist/"+"(Hist)"+titlename)
+
+    plt.show()
+
+def MakeBox(clustering1,clustering2, bot_or_Human1, bot_or_Human2, Save_or_Not=0):
+
+    if (bot_or_Human1 =='Human'):
+        datax = cluster_result_df.query('cluster == %s & Type=="Human"'%clustering1).iloc[:,0:-2].as_matrix()
+    elif(bot_or_Human1 =='Bot'):
+        datax = cluster_result_df.query('cluster == %s & Type=="Bot"'%clustering1).iloc[:,0:-2].as_matrix()
+
+    if (bot_or_Human2 == 'Human'):
+        datay = cluster_result_df.query('cluster == %s & Type=="Human"'%clustering2).iloc[:,0:-2].as_matrix()
+
+    elif (bot_or_Human2 == 'Bot'):
+        datay = cluster_result_df.query('cluster == %s & Type=="Bot"'%clustering2).iloc[:,0:-2].as_matrix()
+
+    titlename = '%s_%s & %s_%s' % (bot_or_Human1,clustering1, bot_or_Human2, clustering2)
+    plt.figure(1)
+    plt.subplot(121)
+    #plt.gca().set_ylim([-2, 4])
+    plt.title('cluster: %s & Type: %s'%(clustering1, bot_or_Human1))
+    plt.boxplot(datax, labels=features[:-1])
+    plt.subplot(122)
+    #plt.gca().set_ylim([-2, 4])
+    plt.title('cluster: %s & Type: %s'%(clustering2, bot_or_Human2))
+    plt.boxplot(datay, labels=features[:-1])
+
+    if (Save_or_Not==1):
+        plt.savefig("EDAPicture/Hist/"+"(Box)"+titlename)
+    plt.show()
+
+
+if __name__ == "__main__":
+    #MakeHist(0,1,'Human', 't_deg', 'Bot', 't_deg')
+    MakeBox(1,2,'Bot', 'Bot')
 
--- EDA/EDAmodule/eda_Hist.py
+++ EDA/EDAmodule/eda_Hist.py
@@ -5,28 +5,31 @@ import numpy as np
 
 NetworkDF = pd.read_csv('../log/(after) Network measures features.csv')
 PinfoDF = pd.read_csv('../log/(after) Player information features.csv')
+PactionDF = pd.read_csv('../log/(after) Player actions features.csv')
+GactiveDF = pd.read_csv('../log/(after) Group activities features.csv')
+SointerDF = pd.read_csv('../log/(after) Social interaction diversity features.csv')
 
-NetworkHumanDF = NetworkDF.loc[NetworkDF['Type'] == "Human"]
-NetworkBotDF = NetworkDF.loc[NetworkDF['Type'] == "Bot"]
-MaxLevelHuman = PinfoDF.query('Type == "Human" & Max_level == 55')
-MaxLevelBot = PinfoDF.query('Type == "Bot" & Max_level == 55')
+mergetmp = pd.merge(NetworkDF.iloc[:,0:-1], PinfoDF.drop(['A_Acc'], axis =1).iloc[:,0:-1], on='Actor', how='inner')
+mergetmp = pd.merge(mergetmp, PactionDF.drop(['A_Acc'], axis =1).iloc[:,0:-1], on='Actor', how='inner')
+mergetmp = pd.merge(mergetmp, GactiveDF.drop(['A_Acc'], axis =1).iloc[:,0:-1], on='Actor', how='inner')
+mergerDF = pd.merge(mergetmp, SointerDF.drop(['A_Acc'], axis =1), on='Actor', how='inner')
 
 
-MaxLevelHuman_Actor = MaxLevelHuman[['Actor']]
-MaxLevelBot_Actor = MaxLevelBot[['Actor']]
 
-Bot_Net_Info_merge = pd.merge(MaxLevelBot_Actor, NetworkBotDF, on='Actor', how='inner')
-Human_Net_Info_merge = pd.merge(MaxLevelHuman_Actor, NetworkHumanDF, on='Actor', how='inner')
+
+
+HumanDF = mergerDF.query('Type == "Human" & Max_level == 55')
+BotDF = mergerDF.query('Type == "Bot" & Max_level == 55')
 
 def MakeHist(bot_or_Human1, compareV1, bot_or_Human2, compareV2, Save_or_Not=0):
 
     compare1 = str(compareV1)
     compare2 = str(compareV2)
 
-    Human_compare1 = Human_Net_Info_merge[compare1]
-    Human_compare2 = Human_Net_Info_merge[compare2]
-    Bot_compare1 = Bot_Net_Info_merge[compare1]
-    Bot_compare2 = Bot_Net_Info_merge[compare2]
+    Human_compare1 = HumanDF[compare1]
+    Human_compare2 = HumanDF[compare2]
+    Bot_compare1 = BotDF[compare1]
+    Bot_compare2 = BotDF[compare2]
 
     if (bot_or_Human1 =='Human'):
         x = Human_compare1
@@ -40,6 +43,7 @@ def MakeHist(bot_or_Human1, compareV1, bot_or_Human2, compareV2, Save_or_Not=0):
 
     bins = np.linspace(-10, 100, 100)
     titlename = '%s_%s & %s_%s'%(bot_or_Human1, compare1, bot_or_Human2, compare2)
+
     plt.hist(x, bins, alpha=0.5, label='%s_%s'%(bot_or_Human1, compare1), color = "b")
     plt.hist(y, bins, alpha=0.5, label='%s_%s'%(bot_or_Human2, compare2), color = "r")
     plt.legend(loc='upper right')
